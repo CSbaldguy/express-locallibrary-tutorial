b324b7e68eda25ac9ae21ccd252d3d8e4d20e27e
Gavin Cheung <nmgcheung@gmail.com>
2021-08-12 13:05:17 +0800

    complete challenge in tutorial part 5 - genre list page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -2,7 +2,12 @@ var Genre = require('../models/genre');
 
 // Display list of all Genre.
 exports.genre_list = (req, res) => {
-    res.send('NOT IMPLEMENTED: Genre list');
+    Genre.find()
+        .sort([['name', 'ascending']])
+        .exec((err, list_genres) => {
+            if (err) return next(err);
+            else res.render('genre_list', { title: 'Genre List', genre_list: list_genres });
+        });
 };
 
 // Display detail page for a specific Genre.
